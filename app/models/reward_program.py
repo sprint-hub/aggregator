ffd1937623171aba4afab06716af62ab39dc2b2f
MAC <ksprint4@gmail.com>
2026-07-27 15:43:11 +0100

    auth done

--- app/models/reward_program.py
+++ app/models/reward_program.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Float, DateTime, Boolean, Enum
+from sqlalchemy import Column, String, Float, DateTime, Boolean, Enum, Integer
 from app.models.base import BaseModel
 import enum
 
